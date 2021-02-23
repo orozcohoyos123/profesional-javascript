b347fbb575005df06f686572a3a2c9974114ece9
Sebastian Orozco Hoyos <sebastian.orozco@sinco.com.co>
2021-02-23 16:37:24 -0500

    funciones e interfaces typescript

--- ejercicios/typescript/index.ts
+++ ejercicios/typescript/index.ts
@@ -46,3 +46,57 @@ let comodin: any = 'joker'
 comodin = { type: 'Wildcard'}
 
 let someObject: object = {type: 'algo'}
+
+
+//funciones
+function add(a: number, b: number) : number{
+    return a + b
+}
+
+const suma = add(5, 6)
+
+function createAdder(a: number): (number) => number{
+    return function(b: number){
+        return a + b
+    }
+}
+
+const addFour = createAdder(4)
+const fourPlusSix = addFour(6)
+
+
+function optionalLastName(firstName: string, lastName?: string): string {
+    return `${firstName } ${lastName}`
+}
+
+const sebas = optionalLastName('Sebastian')
+
+function autocompleteLastName(firstName: string, lastName: string = "orozco"): string {
+    return `${firstName } ${lastName}`
+}
+
+const sebas2 = autocompleteLastName('Sebastian')
+
+
+///interfaces
+interface Rectangulo {
+    ancho: number,
+    largo: number
+}
+
+let rectangulo: Rectangulo = {
+    ancho: 4,
+    largo: 6
+}
+
+function areaRectangulo(r: Rectangulo){
+    return r.ancho * r.largo
+}
+console.log(areaRectangulo(rectangulo))
+console.log(rectangulo.toString())
+
+rectangulo.toString = function (){
+    return `Rectangulo de largo: ${this.largo} y ancho: ${this.ancho}`
+}
+
+console.log(rectangulo.toString())
