868c9de1b4e18fbda4493a4ad9be7ff826b0d2e8
sorozco <orozcohoyos123@gmail.com>
2021-02-24 23:39:09 -0500

    finalizacion platzi video en typescript

--- assets/index.ts
+++ assets/index.ts
@@ -1,6 +1,7 @@
 import MediaPlayer from "./MediaPlayer";
 import AutoPlay from "./plugins/AutoPlay";
 import AutoPause from "./plugins/AutoPause";
+import Ads from './plugins/Ads'
 
 const video :HTMLMediaElement = document.querySelector("video");
 const button_play :HTMLElement = document.getElementById("btnPlay");
@@ -10,15 +11,16 @@ const player = new MediaPlayer({
     el: video, 
     plugins: [
         new AutoPlay(),
-        new AutoPause()
+        new AutoPause(),
+        new Ads()
     ]
 });
 button_play.onclick = () => player.togglePlay();
 button_mute.onclick = () => player.toggleMute();
 
 
- if('serviceWorker' in navigator){ //es un tipo de cache offline
+/*  if('serviceWorker' in navigator){ //es un tipo de cache offline
     navigator.serviceWorker.register('../sw.js')
         .catch(err => console.log(err.message))
-} 
+}  */
  // no sirve con parcel, no se por que
